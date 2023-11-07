8de84c55c7c64eeab4cdff6560b41fcccfa2282e
Ashikur-ai <ashikurrahman01755450127@gmail.com>
2023-11-07 20:45:02 +0600

    search button implemented

--- src/pages/AllBlogs/AllBlogs.jsx
+++ src/pages/AllBlogs/AllBlogs.jsx
@@ -5,16 +5,49 @@ import BlogCard from './BlogCard';
 const AllBlogs = () => {
     const blogs = useLoaderData();
     const [categoryPost, setCategoryPost] = useState([]);
+    const [searchTitle, setSearchTitle] = useState("");
+    
+
+    const handleSearch = (event) => {
+        setSearchTitle(event.target.value);
+    }
+
+    const handleSearchButton = () => {
+        const searchedBlog = blogs.filter(blog => blog.blogName.toLowerCase().includes(searchTitle.toLowerCase())
+        );
+        setCategoryPost(searchedBlog)
+    }
+
+    
+
+
     const handleCategory = (category) => {
-        console.log(category);
-        console.log(blogs);
+        
         const filteredPosts = blogs.filter(blog => blog.category === category)
         setCategoryPost(filteredPosts);
     }
+
+    // let resultBlog = [];
+    //  if (categoryPost.length) {
+    //     resultBlog = categoryPost;
+    // }
+    // else if (searchedBlog.length) {
+    //     resultBlog = searchedBlog;
+    // }
+    // else if (blogs.length) {
+    //     resultBlog = blogs;
+    // }
+    
      
     return (
         <>
-            
+            <input type="text"
+                className='border border-lime-500'
+                placeholder='Search your blog post'
+                value={searchTitle}
+                onChange={handleSearch}
+            />
+            <button className='btn btn-primary' onClick={handleSearchButton}>Search</button>
             <div className='grid grid-cols-4'>
                 <button onClick={() => handleCategory("Cybersecurity")}>CyberSecurity</button>
                 <button onClick={() => handleCategory("Technology Trends")}>Technology Trends</button>
@@ -24,14 +57,18 @@ const AllBlogs = () => {
 
 
             <div className='grid grid-cols-3 container mx-auto'>
-                
-
+             
                 {
                     categoryPost.length ?
                         categoryPost?.map(blog => <BlogCard key={blog._id} blog={blog}></BlogCard>)
-                        :
-                        blogs?.map(blog => <BlogCard key={blog._id} blog={blog}></BlogCard>)
+                    : blogs?.map(blog => <BlogCard key={blog._id} blog={blog}></BlogCard>)
                 }
+                
+
+                
+
+              
+                    
                 
                         
                         
