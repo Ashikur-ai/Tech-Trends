9686202931fa5bc8709eca329a6210c33b302945
Ashikur-ai <ashikurrahman01755450127@gmail.com>
2023-11-08 14:49:22 +0600

    homepage and allblog page responsive done

--- src/pages/AllBlogs/AllBlogs.jsx
+++ src/pages/AllBlogs/AllBlogs.jsx
@@ -45,39 +45,98 @@ const AllBlogs = () => {
             <Helmet>
                 <title>Tech Trends | All blogs</title>
             </Helmet>
-            <input type="text"
-                className='border border-lime-500'
-                placeholder='Search your blog post'
-                value={searchTitle}
-                onChange={handleSearch}
-            />
-            <button className='btn btn-primary' onClick={handleSearchButton}>Search</button>
-            <div className='grid grid-cols-4'>
-                <button onClick={() => handleCategory("Cybersecurity")}>CyberSecurity</button>
-                <button onClick={() => handleCategory("Technology Trends")}>Technology Trends</button>
-                <button onClick={() => handleCategory("Web Development")}>Web Development</button>
-                <button onClick={() => handleCategory("Data Science")}>Data Science</button>
+            <div className="hero  " >
+                
+                <div className="hero-content text-center flex-col">
+                    <img src="https://i.ibb.co/ggW4fhH/Animation-1699424004826.gif" className='' alt="" />
+
+                    <div className="space-y-5">
+
+                        <p className='font-bold text-6xl'>Search Your Desire Blog</p>
+                        <input type="text"
+                            className='input shadow-lg input-bordered w-full max-w-xs'
+                            placeholder='Search your blog post'
+                            value={searchTitle}
+                            onChange={handleSearch}
+                        />
+                        <button onClick={handleSearchButton} className="btn btn-primary ml-3">Search</button>
+                    </div> 
+                    <div>
+                    </div>
+                </div>
+                
             </div>
+            {/* category  */}
+            <div className='container pb-10 mx-auto px-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2'>
 
+                <div onClick={() => handleCategory("Cybersecurity")} className="card bg-base-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                        <img src="https://i.ibb.co/fdz27cQ/cyberlogo.png" alt="Shoes" className="rounded-xl" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Cyber Security</h2>
 
-            <div className='grid grid-cols-3 container mx-auto'>
-             
-                {
-                    categoryPost.length ?
-                        categoryPost?.map(blog => <BlogCard key={blog._id} blog={blog}></BlogCard>)
-                    : blogs?.map(blog => <BlogCard key={blog._id} blog={blog}></BlogCard>)
-                }
-                
+                        <div className="card-actions">
 
-                
+                        </div>
+                    </div>
+                </div>
 
-              
-                    
-                
-                        
-                        
-                    
+                <div onClick={() => handleCategory("Technology Trends")} className="card bg-base-100 shadow-2xl">
+                    <figure className="px-10 pt-10">
+                        <img src="https://i.ibb.co/wB11hsF/techlogo.png" alt="Shoes" className="rounded-xl" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Technology Trends</h2>
+
+                        <div className="card-actions">
+
+                        </div>
+                    </div>
+                </div>
+
+                <div onClick={() => handleCategory("Web Development")} className="card bg-base-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                        <img src="https://i.ibb.co/dJ3cqLT/weblogo.png" alt="Shoes" className="rounded-xl" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Web Development</h2>
+
+                        <div className="card-actions">
+
+                        </div>
+                    </div>
+                </div>
+
+                <div onClick={() => handleCategory("Data Science")} className="card bg-base-100 shadow-2xl">
+                    <figure className="px-10 pt-10">
+                        <img src="https://i.ibb.co/qFN58wQ/datalogo.png" alt="Shoes" className="rounded-xl" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Data Science</h2>
+
+                        <div className="card-actions">
+
+                        </div>
+                    </div>
+                </div>
+
             </div>
+            
+            
+            <div className='container mx-auto'>
+                <p className=' font-bold text-3xl mb-10'>Explore our blogs</p>
+
+                <div className='grid grid-cols-1 md:grid-cols-2 mx-5 lg:grid-cols-3 gap-6 '>
+
+                    {
+                        categoryPost.length ?
+                            categoryPost?.map(blog => <BlogCard key={blog._id} blog={blog}></BlogCard>)
+                            : blogs?.map(blog => <BlogCard key={blog._id} blog={blog}></BlogCard>)
+                    }
+                </div>
+            </div>
+            
         </>
         
     );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,10 +5,14 @@ import toast from 'react-hot-toast';
 import { Helmet } from 'react-helmet-async';
 import { motion } from "framer-motion";
 import BlogCard from '../AllBlogs/BlogCard';
+import InterviewCard from './InterviewCard';
 
 const Home = () => {
     
     const blogs = useLoaderData();
+    const handleNewsLetter = () => {
+        toast.success("Thank you for subscribing to our newsletter")
+    }
     return (
         <div className=' min-h-screen'>
             <Helmet>
@@ -22,7 +26,7 @@ const Home = () => {
                     <p className='border-l-2 border-black p-3'>On this website we share tips and tricks, frameworks, projects, tutorials, etc <br />
                         Make sure you subscribe to get the latest updates</p>
                     <input type="text" placeholder="Enter your email here" className="input shadow-lg input-bordered w-full max-w-xs" />
-                    <button className='btn btn-primary ml-2'>Subscribe</button>
+                    <button onClick={handleNewsLetter} className='btn btn-primary ml-2'>Subscribe</button>
                 </div>
                 <motion.div animate={{ scale: 1 }}
                     initial={{ scale: 0 }}>
@@ -30,6 +34,71 @@ const Home = () => {
                 </motion.div>
             </div>
 
+            {/* Browse category  */}
+            <div className='bg-white pt-11'>
+                <div className=' flex items-center gap-1 text-2xl font-bold p-2 container mx-auto'>
+                    <p>Our Community </p>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="5" viewBox="0 0 35 5" fill="none">
+                        <line y1="3.5" x2="35" y2="3.5" stroke="#2B2C34" stroke-width="2" />
+                    </svg>
+                </div>
+                <div className='container mx-auto px-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2'>
+
+                    <div className="card bg-base-100 shadow-xl">
+                        <figure className="px-10 pt-10">
+                            <img src="https://i.ibb.co/bWdQH70/techtribe.png" alt="Shoes" className="rounded-xl" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">TechTribe</h2>
+                           
+                            <div className="card-actions">
+                                
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="card bg-base-100 shadow-2xl">
+                        <figure className="px-10 pt-10">
+                            <img src="https://i.ibb.co/DbLQsqd/digital-explorer.png" alt="Shoes" className="rounded-xl" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Digital Explorer</h2>
+
+                            <div className="card-actions">
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="card bg-base-100 shadow-xl">
+                        <figure className="px-10 pt-10">
+                            <img src="https://i.ibb.co/gw4jGn5/datawizard.png" alt="Shoes" className="rounded-xl" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Data Wizard</h2>
+
+                            <div className="card-actions">
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="card bg-base-100 shadow-2xl">
+                        <figure className="px-10 pt-10">
+                            <img src="https://i.ibb.co/9GNKCCJ/geek.jpg" alt="Shoes" className="rounded-xl" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Geek Hub</h2>
+
+                            <div className="card-actions">
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            
             {/* Recent Posts  */}
             <div className='bg-white pt-11'>
                 <div className=' flex items-center gap-1 text-2xl font-bold p-2 container mx-auto'>
@@ -38,7 +107,7 @@ const Home = () => {
                         <line y1="3.5" x2="35" y2="3.5" stroke="#2B2C34" stroke-width="2" />
                     </svg>
                 </div>
-                <div className='container mx-auto px-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2'>
+                <div className='container mx-auto px-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2'>
                     {
                         blogs.slice(-6)?.map(blog => <BlogCard key={blog._id} blog={blog}></BlogCard>)
                     }
@@ -56,11 +125,42 @@ const Home = () => {
                     <p className='text-center text-gray-300'>Subscribe to newsletter and never miss the new post every week.</p>
                     <div className='flex justify-center'>
                         <input type="text" placeholder="Enter your email here" className="input shadow-lg input-bordered w-full max-w-xs" />
-                        <button className='btn btn-primary ml-2'>Subscribe</button>
+                        <button onClick={handleNewsLetter} className='btn btn-primary ml-2'>Subscribe</button>
                     </div>
                 </div>
             </div>
            
+            
+            {/* Recent Posts  */}
+            <div className='bg-white pt-11'>
+                <div className=' flex items-center gap-1 text-2xl font-bold p-2 container mx-auto'>
+                    <p>Recent Interviews </p>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="5" viewBox="0 0 35 5" fill="none">
+                        <line y1="3.5" x2="35" y2="3.5" stroke="#2B2C34" stroke-width="2" />
+                    </svg>
+                </div>
+                <div className='container mx-auto px-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 pb-5'>
+
+                    <InterviewCard
+                        img={"https://i.ibb.co/7Xs6RkH/inter1.jpg"}
+                        name={"Elon Musk"}
+                        position={"CEO and chief technology officer of SpaceX"}
+                    ></InterviewCard>
+                    <InterviewCard
+                        img={"https://i.ibb.co/fNf70BX/inter3.jpg"}
+                        name={"Jack Ma"}
+                        position={"Chinese business magnate and investor"}
+                    ></InterviewCard>
+
+                    <InterviewCard
+                        img={"https://i.ibb.co/FmH1P6y/inter4.jpg"}
+                        name={"Jeff Bezos"}
+                        position={"CEO of Amazon"}
+                    >
+                    </InterviewCard>
+
+                </div>
+            </div>
             
 
             
